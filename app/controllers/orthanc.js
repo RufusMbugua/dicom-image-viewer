9c5f3a4ad7aef7be4948099179aed564d9cca28a
Collins Ojenge <collinsojenge@gmail.com>
2016-08-22 19:32:41 +0300

    Using Async to get Patient Data:

--- app/controllers/orthanc.js
+++ app/controllers/orthanc.js
@@ -1,69 +1,64 @@
+/**
+ * Declarations
+ */
+// Packages
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var async = require('async');
 
+// Variables
 var orthanc = 'http://orthanc.rufusmbugua.com/';
+var patients = [];
 
-
-var patient = [];
-
-/* GET home page. */
+/* GET Patients List */
 router.get('/patients', function(req, res, next) {
+  async.waterfall([
+    get_patients,
+    get_patient,
+  ],
+  function (err, result) {
+    patients = result;
+    res.json(result)
+  });
+});
 
-  var patient;
+/**
+* [get_patients description]
+* @param  {[type]} waterfallCallback [description]
+* @return {[type]}                   [description]
+*/
+function get_patients(waterfallCallback){
   request(orthanc + 'patients')
   .on('data',function(data){
     patients = JSON.parse(data);
   })
   .on('end',function(){
-    var patientsData=[];
-    for (i = 0; i < patients.length; i++) {
-      patientId = patients[i];
-      request(orthanc + 'patients/' + patientId)
-      .on('data',function(data){
-        patient = JSON.parse(data);
-      })
-      .on('end',function(){
-        patientsData.push(patient)
-        if(patientsData.length==patients.length){
-          res.json(patientsData)
-        }
-      })
+    waterfallCallback(null,patients)
+  })
+}
 
+/**
+* [get_patient description]
+* @param  {[type]} patients [description]
+* @return {[type]}          [description]
+*/
+function get_patient(patients,callback){
+
+  async.map(patients, function(patientId, callback) {
+    request(orthanc + 'patients/' + patientId,function(err, response, body){
+      if (err) {
+        return callback(err);
+      }
+      callback(null, JSON.parse(body));
+    })
+  }, function(err, extended) {
+    if (err) {
+      // handle error
     }
-
-  })
-
-});
-
-router.get('/dicom', function(req, res, next) {
-
-});
-
-router.get('/patients', function(req, res, next) {
-
-});
-
-function format_patient_data(patientArray) {
-  patientArray = JSON.parse(patientArray)
-  return patientArray;
-
-  for (i = 0; i < patientArray.length; i++) {
-    patientId = patientArray[i];
-    patient = get_patient(patientId);
-    // patients.push(patient)
-  }
-  // return patients;
-}
-
-function get_patients(){
-
-}
-function get_patient(patientId){
-  request(orthanc + 'patients/' + patientId)
-  .on('data',function(data){
-    patient
-  })
+    // extended is an array containing the parsed JSON
+    callback(null,extended)
+  });
 }
 
 module.exports = router;
